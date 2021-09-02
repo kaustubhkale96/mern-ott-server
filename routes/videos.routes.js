e0d4d12a5a4b20c67cf6568bc7d24d349aec0b8e
Kaustubh Kale <kaustubhkale96@gmail.com>
2021-09-02 23:34:51 +0530

    Edit & Delete Added

--- routes/videos.routes.js
+++ routes/videos.routes.js
@@ -46,4 +46,51 @@ router.get('/get', (req, res) => {
         });
 });
 
+router.get('/get/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(req.params)
+    Videos.findOne({ _id: id })
+        .then(data => {
+            console.log(data)
+            if (!data) {
+                res.status(404).send({ message: `Cannot find data for ${id}` })
+            }
+            else res.send(data)
+        })
+        .catch(err => {
+            res.status(500).send({ message: err || `Some Error Occrured` })
+        })
+})
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    if (!req.body) {
+        res.satus(500).send({ message: `Data cannot be empty` })
+    }
+    Videos.findOneAndUpdate({ _id: id }, req.body, { useFindAndModify: false, new: true })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({ message: `Cannot update data of ${id}` })
+            }
+            else res.send({ message: `Edited Successfully!` })
+        })
+        .catch((err) => {
+            res.status(500).send({ message: err || `Some Error occured` })
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    Videos.findByIdAndDelete({ _id: id })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({ message: `Cannot data delete of ${id}` })
+            }
+            else res.send({ message: `Deleted Successfully` })
+        })
+        .catch(err => {
+            res.status(500).send({ message: err || 'Some Error Occured' })
+        })
+})
+
 module.exports = router;
